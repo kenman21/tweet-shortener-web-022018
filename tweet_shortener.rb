c3302d485cf114d2f9813d0984f706299babfe09
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-07 15:16:58 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+
+def dictionary 
+  word_hash = {
+    "hello" => "hi",
+    "to" => "2",
+    "two"=> "2", 
+    "too"=> "2", 
+    "for" => "4", 
+    "four" => "4", 
+    "be" => "b", 
+    "you" => "u", 
+    "at" => "@", 
+    "and" => "&"
+  }
+end 
+
+def word_substituter(string)
+  array = Array.new 
+  array_new = Array.new 
+  array = string.split()
+  word_hash = dictionary
+  array.map do |words|
+    if word_hash.keys.include?(words)
+      words = word_hash[words]
+    end 
+    array_new.push(words)
+  end
+  array_new.join(' ')
+end 
+  
